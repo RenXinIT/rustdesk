28e666e53dcd6f5b3c36510c92fabb9fd34ea198
程小成 <54830642+RenXinIT@users.noreply.github.com>
2024-12-05 19:34:19 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["vps.dsmynas.org"];
+pub const PUBLIC_RS_PUB_KEY: &str = "Vu82nJWkmwe5+3hM6aYied3O0+b8FJKmj9hYvJqUHKg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
